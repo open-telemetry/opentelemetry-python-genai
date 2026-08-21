1cef84c1184edd17cb3bd261b7eec5132d3439f5
Radhika Gupta <guptaradhika@microsoft.com>
2026-08-21 07:54:23 -0700

    Fix format

--- instrumentation/opentelemetry-instrumentation-genai-openai/src/opentelemetry/instrumentation/genai/openai/utils.py
+++ instrumentation/opentelemetry-instrumentation-genai-openai/src/opentelemetry/instrumentation/genai/openai/utils.py
@@ -172,7 +172,9 @@ def create_chat_invocation(
         invocation.input_messages = _prepare_input_messages(
             kwargs.get("messages", [])
         )
-    invocation.tool_definitions = _prepare_tool_definitions(kwargs.get("tools"))
+    invocation.tool_definitions = _prepare_tool_definitions(
+        kwargs.get("tools")
+    )
     return invocation
 
 
