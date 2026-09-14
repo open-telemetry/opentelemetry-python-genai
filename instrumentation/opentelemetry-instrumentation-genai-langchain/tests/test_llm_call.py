dbd88886cabf5d774e895d41780aae07a9f4a938
Radhika Gupta <guptaradhika@microsoft.com>
2026-09-14 07:52:28 -0700

    Fix formatting

--- instrumentation/opentelemetry-instrumentation-genai-langchain/tests/test_llm_call.py
+++ instrumentation/opentelemetry-instrumentation-genai-langchain/tests/test_llm_call.py
@@ -805,9 +805,7 @@ def test_chat_model_preserves_input_and_output_message_names(
     assert output_messages[0]["role"] == "assistant"
 
     assert gen_ai_attributes.GEN_AI_REQUEST_TOP_K not in span.attributes
-    assert (
-        gen_ai_attributes.GEN_AI_REQUEST_CHOICE_COUNT not in span.attributes
-    )
+    assert gen_ai_attributes.GEN_AI_REQUEST_CHOICE_COUNT not in span.attributes
 
 
 def test_chat_model_captures_top_k_and_choice_count(
@@ -842,9 +840,7 @@ def test_chat_model_captures_top_k_and_choice_count(
 
     (span,) = span_exporter.get_finished_spans()
     assert span.attributes[gen_ai_attributes.GEN_AI_REQUEST_TOP_K] == 40
-    assert (
-        span.attributes[gen_ai_attributes.GEN_AI_REQUEST_CHOICE_COUNT] == 3
-    )
+    assert span.attributes[gen_ai_attributes.GEN_AI_REQUEST_CHOICE_COUNT] == 3
 
     (log,) = log_exporter.get_finished_logs()
     assert (
